e2561a331acf23ae4eba532931e9c8bf70b6dcc5
WGR050505 <2415853219@qq.com>
2024-12-14 14:00:20 +0800

    Site updated: 2024-12-14 14:00:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/13/CBbse/","2024/12/11/hello-world/"];function toRandomPost(){
+var posts=["2024/12/13/CBbse/","2024/12/14/浏览器的开发者工具/","2024/12/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
