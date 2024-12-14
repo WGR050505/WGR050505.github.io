20a48d8ebefc93f4b34fd7a791c8d33410f616d9
WGR050505 <2415853219@qq.com>
2024-12-14 11:53:59 +0800

    Site updated: 2024-12-14 11:53:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/11/hello-world/","2024/12/13/CBbse/"];function toRandomPost(){
+var posts=["2024/12/13/CBbse/","2024/12/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
