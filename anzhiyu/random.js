a9cc8b914c9615b7bf28b6f181f821813a63ad95
WGR050505 <2415853219@qq.com>
2024-12-16 23:38:31 +0800

    Site updated: 2024-12-16 23:38:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/13/CBbse/","2024/12/14/浏览器的开发者工具/","2024/12/16/Bash介绍/","2024/12/11/hello-world/"];function toRandomPost(){
+var posts=["2024/12/13/CBbse/","2024/12/14/浏览器的开发者工具/","2024/12/16/Bash介绍/","2024/12/11/hello-world/","2024/12/16/Javascript基础入门教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
